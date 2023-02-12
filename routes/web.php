bbcdf39a3dffcd4b5e6b7dd8cb00bf30a4152cde
nadhiaaprina <nadhiaaprina7@gmail.com>
2023-02-12 12:44:03 +0700

    perubahan route untuk pemesanan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PemesananController;
+use App\Http\Controllers\PelangganController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,21 @@ Route::get('/login', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    // manajemen jurusan
+    Route::get('/pemesanan', [PemesananController::class, 'index']);
+    Route::get('/pemesanan/form', [PemesananController::class, 'create']);
+    Route::post('/pemesanan', [PemesananController::class, 'store']);
+    Route::get('/pemesanan/edit/{id}', [PemesananController::class, 'edit']);
+    Route::put('/pemesanan/{id}', [PemesananController::class, 'update']);
+    Route::delete('/pemesanan/{id}', [PemesananController::class, 'destroy']);
+
+    Route::get('/pelanggan', [PelangganController::class, 'index']);
+    Route::get('/pelanggan/form', [PelangganController::class, 'create']);
+    Route::post('/pelanggan', [PelangganController::class, 'store']);
+    Route::get('/pelanggan/edit/{id}', [PelangganController::class, 'edit']);
+    Route::put('/pelanggan/{id}', [PelangganController::class, 'update']);
+    Route::delete('/pelanggan/{id}', [PelangganController::class, 'destroy']);
+
+});
